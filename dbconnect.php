61b6f421a8d3d8d9f14c07e55d3112e4d8cdd21f
kirill <wenitalist@gmail.com>
2021-02-05 19:00:24 +0300

    Первый div

---
+++ dbconnect.php
@@ -0,0 +1,29 @@
+<?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+$host = 'localhost';
+$db   = 'guest_book';
+$user = 'admin';
+$pass = 'secret';
+$port = "3306";
+$charset = 'utf8mb4';
+
+$options = [
+    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
+    \PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+try {
+    $pdo = new \PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+//$stmt = $pdo->query('SELECT * FROM posts');
+//$data = $stmt->fetchAll();
+
+//$stmt = $pdo->query('SELECT title, content, date_of_public, author FROM posts WHERE id = 1');
+//$data = $stmt->fetchAll();
--- index.php
+++ index.php
@@ -1,4 +1,68 @@
 <?php
+    include ('dbconnect.php');
+?>
 
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>guest_book</title>
+    <style>
+        .main {
+            border: solid 2px black;
+            width: 800px;
+            margin: auto;
+        }
+        .addl {
+            border: 4px double blue;
+            margin: 3px;
+        }
+        .text {
+            margin: 3px;
+        }
+        .date {
+            text-align: right;
+            margin-right: 3px;
+            margin-top: 1px;
+            margin-bottom: 6px;
+        }
+    </style>
+</head>
 
-echo 'сам';
+<body>
+<div class="main">
+    <div class="addl">
+        <h2 class="text">
+            <?php
+            global $pdo;
+            $stmt = $pdo->query('SELECT title FROM posts WHERE id = 1');
+            $data = $stmt->fetchAll();
+            echo($data[0]->title);
+            ?>
+        </h2>
+        <hr>
+        <p class="text">
+            <?php
+            global $pdo;
+            $stmt = $pdo->query('SELECT content FROM posts WHERE id = 1');
+            $data = $stmt->fetchAll();
+            echo($data[0]->content);
+            ?>
+        </p>
+        <hr>
+        <p class="date">
+            <?php
+            global $pdo;
+            $stmt = $pdo->query('SELECT author, date_of_public FROM posts WHERE id = 1');
+            $data = $stmt->fetchAll();
+            $str_aut = ($data[0]->author);
+            $str_aut .= " - ";
+            $str_date = ($data[0]->date_of_public);
+            $str_aut .= $str_date;
+            echo($str_aut);
+            ?>
+        </p>
+    </div>
+</div>
+</body>
+</html>
