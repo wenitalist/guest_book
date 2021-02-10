520af6888345cf7e7036139e65d7ab3aabd6a872
kirill <wenitalist@gmail.com>
2021-02-10 12:50:21 +0300

    create table

--- data.example.php
+++ data.example.php
@@ -5,9 +5,3 @@ $db   = "";
 $user = "";
 $pass = "";
 $port = "";
-
-$options = [
-    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ,
-    \PDO::ATTR_EMULATE_PREPARES   => false,
-];
