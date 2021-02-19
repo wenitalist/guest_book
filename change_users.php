88b754b2d4859eaade65110784f2f9137daf2170
kirill <wenitalist@gmail.com>
2021-02-19 13:25:30 +0300

    Design

--- change_users.php
+++ change_users.php
@@ -5,10 +5,26 @@
     }
     .table {
         border-collapse: collapse;
+        font-size: 20px;
+        margin: 2%;
+    }
+    .zxc {
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 15%;
+        width: 575px;
     }
     .del_submit {
-        margin-left: auto;
-        float: left;
+        float: right;
+        margin-top: 1%;
+    }
+    .dop {
+        margin-top: 5px;
+        border: solid 2px black;
+        width: 438px;
+    }
+    a. {
+        margin: auto;
     }
 </style>
 <?php
@@ -21,7 +37,9 @@ $stmt = $pdo->query("SELECT * FROM users");
 //$rows = $stmt->rowCount(); // Кол-во строк в таблице
 $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo("<a href='select_table.php'>Назад</a>");
+echo("<div class='zxc'>");
+echo("<a href='select_table.php' class='a'>Назад</a>");
+echo("<div class='dop'>");
 echo("<form method='POST' action='/script_delete.php'>");
 echo("<table class='table'>");
 echo("<tr>
@@ -50,3 +68,5 @@ echo("</table>");
 echo("<input type='text' name='selected_table' value='users' hidden/>");
 echo("<p><input type='submit' name='delete_rows' class='del_submit' value='Удалить'/></p>");
 echo("</form>");
+echo("</div>");
+echo("</div>");
--- select_table.php
+++ select_table.php
@@ -9,10 +9,33 @@ $data = $stmt->fetchAll();
 <head>
     <meta charset="utf-8">
     <title>Выбрать таблицу</title>
+    <style>
+        .main {
+            border: solid 2px black;
+            width: 13%;
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 15%;
+        }
+        .field {
+            margin: 5px;
+            text-align: center;
+        }
+        .reg {
+            text-align: center;
+            margin-left: 11%;
+        }
+        .tables {
+            margin: 3%;
+            font-size: 20px;
+        }
+    </style>
 </head>
 <body>
-    <div>
-        <a href="index.php">Назад</a>
+<div class="main">
+    <div class="field">
+        <h2 class="field">Выберите таблицу</h2>
+        <hr>
         <?php
         $q = 0;
         while ($data[$q])
@@ -21,9 +44,14 @@ $data = $stmt->fetchAll();
             $str = 'change_';
             $str .= $name_t;
             $str .= '.php';
-            echo("<p><a href='$str'>$name_t</a></p>");
+            echo("<p class='tables'><a href='$str'>$name_t</a></p>");
             $q++;
         }
         ?>
     </div>
+</div>
+<div class="reg">
+    <a href="index.php">Назад</a>
+</div>
 </body>
+</html>
